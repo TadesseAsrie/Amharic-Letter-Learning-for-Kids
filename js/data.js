aa8d9a6943c0b87a4cdbcb0fb9a26611a5966a7f
TadesseAsrie <tademr052@gmail.com>
2026-07-12 14:41:17 -0700

    add more letters

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 /**
- * Core Data Map containing structural character data.
+ * Core Data Map containing the complete set of 33 Primary Amharic Base Letters (ሆሄያት)
  */
 const AMHARIC_ALPHABET = [
   {
@@ -13,11 +13,11 @@ const AMHARIC_ALPHABET = [
       [
         { x: 80, y: 80 },
         { x: 240, y: 80 },
-      ], // Top line path horizontal layout
+      ],
       [
         { x: 160, y: 80 },
         { x: 160, y: 240 },
-      ], // Center stem vertical down path
+      ],
     ],
   },
   {
@@ -43,6 +43,24 @@ const AMHARIC_ALPHABET = [
     ],
   },
   {
+    id: "he_1",
+    letter: "ሐ",
+    pronunciation: "Ha",
+    exampleWord: "ሐመር",
+    exampleMeaning: "Ship",
+    exampleGraphic: "🚢",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+      ],
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
     id: "me_1",
     letter: "መ",
     pronunciation: "Me",
@@ -62,14 +80,46 @@ const AMHARIC_ALPHABET = [
         { x: 240, y: 80 },
         { x: 240, y: 240 },
       ],
-      [
-        { x: 80, y: 160 },
-        { x: 240, y: 160 },
-      ],
     ],
   },
   {
     id: "se_1",
+    letter: "ሠ",
+    pronunciation: "Se",
+    exampleWord: "ሠረገላ",
+    exampleMeaning: "Chariot",
+    exampleGraphic: "🛺",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+      [
+        { x: 220, y: 80 },
+        { x: 220, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "re_1",
+    letter: "ረ",
+    pronunciation: "Re",
+    exampleWord: "ርግብ",
+    exampleMeaning: "Dove",
+    exampleGraphic: "🕊️",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 140 },
+      ],
+      [
+        { x: 220, y: 140 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "se_2",
     letter: "ሰ",
     pronunciation: "Se",
     exampleWord: "ሰዓት",
@@ -87,6 +137,371 @@ const AMHARIC_ALPHABET = [
       [
         { x: 220, y: 140 },
         { x: 220, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "she_1",
+    letter: "ሸ",
+    pronunciation: "She",
+    exampleWord: "ሸረሪት",
+    exampleMeaning: "Spider",
+    exampleGraphic: "🕷️",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+      [
+        { x: 220, y: 80 },
+        { x: 220, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "qe_1",
+    letter: "ቀ",
+    pronunciation: "Qe",
+    exampleWord: "ቀለበት",
+    exampleMeaning: "Ring",
+    exampleGraphic: "💍",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "be_1",
+    letter: "በ",
+    pronunciation: "Be",
+    exampleWord: "በግ",
+    exampleMeaning: "Sheep",
+    exampleGraphic: "🐑",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+        { x: 220, y: 240 },
+        { x: 220, y: 80 },
+      ],
+    ],
+  },
+  {
+    id: "te_1",
+    letter: "ተ",
+    pronunciation: "Te",
+    exampleWord: "ተኩላ",
+    exampleMeaning: "Wolf",
+    exampleGraphic: "🐺",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+      ],
+    ],
+  },
+  {
+    id: "che_1",
+    letter: "ቸ",
+    pronunciation: "Che",
+    exampleWord: "ቸኮሌት",
+    exampleMeaning: "Chocolate",
+    exampleGraphic: "🍫",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+      ],
+    ],
+  },
+  {
+    id: "ha_2",
+    letter: "ኀ",
+    pronunciation: "Ha",
+    exampleWord: "ኀብስት",
+    exampleMeaning: "Bread",
+    exampleGraphic: "🍞",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "ne_1",
+    letter: "ነ",
+    pronunciation: "Ne",
+    exampleWord: "ነብር",
+    exampleMeaning: "Leopard",
+    exampleGraphic: "🐆",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "nye_1",
+    letter: "ኘ",
+    pronunciation: "Nye",
+    exampleWord: "ኘውኘው",
+    exampleMeaning: "Cat Sound",
+    exampleGraphic: "🐱",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "ae_1",
+    letter: "አ",
+    pronunciation: "A",
+    exampleWord: "አንበሳ",
+    exampleMeaning: "Lion",
+    exampleGraphic: "🦁",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "ke_1",
+    letter: "ከ",
+    pronunciation: "Ke",
+    exampleWord: "ከረሜላ",
+    exampleMeaning: "Candy",
+    exampleGraphic: "🍬",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "khe_1",
+    letter: "ኸ",
+    pronunciation: "Khe",
+    exampleWord: "ኸሚስ",
+    exampleMeaning: "Thursday",
+    exampleGraphic: "📅",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "we_1",
+    letter: "ወ",
+    pronunciation: "We",
+    exampleWord: "ወፍ",
+    exampleMeaning: "Bird",
+    exampleGraphic: "🐦",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "ae_2",
+    letter: "ዐ",
+    pronunciation: "A",
+    exampleWord: "ዓይን",
+    exampleMeaning: "Eye",
+    exampleGraphic: "👁️",
+    strokes: [[{ x: 160, y: 160 }]],
+  },
+  {
+    id: "ze_1",
+    letter: "ዘ",
+    pronunciation: "Ze",
+    exampleWord: "ዘሆን",
+    exampleMeaning: "Elephant",
+    exampleGraphic: "🐘",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+        { x: 100, y: 240 },
+        { x: 220, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "zhe_1",
+    letter: "ዠ",
+    pronunciation: "Zhe",
+    exampleWord: "ዠንጥላ",
+    exampleMeaning: "Umbrella",
+    exampleGraphic: "☂️",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+      ],
+    ],
+  },
+  {
+    id: "ye_1",
+    letter: "የ",
+    pronunciation: "Ye",
+    exampleWord: "የካቲት",
+    exampleMeaning: "February",
+    exampleGraphic: "📅",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "de_1",
+    letter: "ደ",
+    pronunciation: "De",
+    exampleWord: "ደመና",
+    exampleMeaning: "Cloud",
+    exampleGraphic: "☁️",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "jje_1",
+    letter: "ጀ",
+    pronunciation: "Je",
+    exampleWord: "ጅብ",
+    exampleMeaning: "Hyena",
+    exampleGraphic: "🐆",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "ge_1",
+    letter: "ገ",
+    pronunciation: "Ge",
+    exampleWord: "ገበታ",
+    exampleMeaning: "Tray",
+    exampleGraphic: "🍱",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "te_2",
+    letter: "ጠ",
+    pronunciation: "T'e",
+    exampleWord: "ጠረጴዛ",
+    exampleMeaning: "Table",
+    exampleGraphic: "🪑",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "che_2",
+    letter: "ጨ",
+    pronunciation: "Ch'e",
+    exampleWord: "ጨረቃ",
+    exampleMeaning: "Moon",
+    exampleGraphic: "🌙",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "pe_1",
+    letter: "ጰ",
+    pronunciation: "P'e",
+    exampleWord: "ጰራቅሊጦስ",
+    exampleMeaning: "Pentecost",
+    exampleGraphic: "⛪",
+    strokes: [
+      [
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "tse_1",
+    letter: "ጸ",
+    pronunciation: "Ts'e",
+    exampleWord: "ጸሐይ",
+    exampleMeaning: "Sun",
+    exampleGraphic: "☀️",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+      ],
+    ],
+  },
+  {
+    id: "tse_2",
+    letter: "ፀ",
+    pronunciation: "Ts'e",
+    exampleWord: "ፀዳል",
+    exampleMeaning: "Ray/Glow",
+    exampleGraphic: "✨",
+    strokes: [[{ x: 160, y: 160 }]],
+  },
+  {
+    id: "fe_1",
+    letter: "ፈ",
+    pronunciation: "Fe",
+    exampleWord: "ፈረስ",
+    exampleMeaning: "Horse",
+    exampleGraphic: "🐎",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 100, y: 240 },
+        { x: 220, y: 80 },
+      ],
+    ],
+  },
+  {
+    id: "pe_2",
+    letter: "ፐ",
+    pronunciation: "Pe",
+    exampleWord: "ፖሊስ",
+    exampleMeaning: "Police",
+    exampleGraphic: "👮",
+    strokes: [
+      [
+        { x: 100, y: 80 },
+        { x: 220, y: 80 },
+        { x: 160, y: 80 },
+        { x: 160, y: 240 },
       ],
     ],
   },
